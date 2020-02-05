0c6ad1e8b477a45a3290afd2033af2855d461530
Devbuilt <leo@devbuiltlabs.com>
2020-02-04 18:21:36 -0800

    first commit

---
+++ DogOpentrons.py
@@ -0,0 +1,56 @@
+from opentrons import protocol_api
+
+# metadata
+metadata = {
+
+    'apiLevel': '2.0',
+    'protocolName': 'Dog Demo',
+    'author': 'NoviceDesigner',
+    'source': 'based off of dinosaur in Protocol Library'
+    }
+
+# a 12 row trough for sources
+trough = labware.load('trough-12row', 8)
+
+# plate to create dog in
+plate = labware.load('96-PCR-flat', 3)
+
+# a tip rack for our pipette
+p200rack = labware.load('tiprack-200ul', 6)
+
+# wells to dispense dog body in yellow
+yellow_wells = [
+    well.bottom() for well in plate.wells(
+        'C2', 'B3', 'C3', 'D3', 'H3', 'B4', 'C4', 'E4',
+        'F4', 'G4', 'D5', 'E5', 'F5', 'D6', 'E6', 'D7',
+        'E7', 'D8', 'E8', 'D9', 'E9', 'F9', 'H9', 'D10',
+        'E10', 'F10', 'G10', 'C11', 'A12', 'B12')]
+
+# wells to dispense dog collar in red
+red_wells = [
+    well.bottom() for well in plate.wells(
+        'E3', 'D4')]
+
+# yellow solution location
+yellow = trough.wells('A1')
+# red solution location
+red = trough.wells('A2')
+
+
+def run_custom_protocol(
+        pipette_axis: 'StringSelection...'='left'):
+
+    p300 = instruments.P300_Single(
+        mount=pipette_axis,
+        tip_racks=[p200rack]
+    )
+
+    # macro commands like .distribute() make writing long sequences easier:
+    # distribute yellow solution to the body
+    p300.distribute(50, yellow, yellow_wells, disposal_vol=0, blow_out=True)
+    # distribute red solution to the dog's collar
+    p300.distribute(50, red, blue_wells, disposal_vol=0, blow_out=True)
+
+
+run_custom_protocol(**{'pipette_axis': 'left'})
+
---
+++ Humming.py
@@ -0,0 +1,25 @@
+from opentrons import protocol_api
+
+metadata = {
+
+    'apiLevel': '2.0',
+    'protocolName': 'Hummingbird Demo',
+    'author': 'NoviceDesigner',
+    'source': 'based off of dinosaur in Protocol Library'
+    }
+
+# a 12 row trough for sources
+trough = labware.load('trough-12row', 8)
+
+# plate to create hummingbird in
+plate = labware.load('96-PCR-flat', 3)
+
+# a tip rack for our pipette
+p200rack = labware.load('tiprack-200ul', 6)
+
+# wells to dispense hummingbird body in red
+red_wells = [well.bottom() for well in plate.wells(
+        'C1', 'G1', 'B2', 'C2', 'G2', 'H2', 'A3', 'B3',
+        'C3', 'G3', 'A4', 'B4', 'C4', 'F4', 'G4', 'B5',
+        'C5', 'D5', 'E5', 'F5', 'C6', 'D6', 'B7', 'C7',
+        'B8', 'C8', 'D8', 'B9', 'D9', 'E9','B10', 'D10','E10', 'F10', 'B11', 'E11','F11', 'G11','B12')]
