18591415249cf2243471bca9923c4cb8e9ed6af1
Devbuilt <leo@devbuiltlabs.com>
2020-02-07 15:52:20 -0800

    humming

--- HummingBirdOpentrons.py
+++ HummingBirdOpentrons.py
@@ -24,3 +24,8 @@ red_wells = [well.bottom() for well in plate.wells(
         'C3', 'G3', 'A4', 'B4', 'C4', 'F4', 'G4', 'B5',
         'C5', 'D5', 'E5', 'F5', 'C6', 'D6', 'B7', 'C7',
         'B8', 'C8', 'D8', 'B9', 'D9', 'E9','B10', 'D10','E10', 'F10', 'B11', 'E11','F11', 'G11','B12')]
+
+        # wells to dispense hummingbird belly in green
+green_wells = [
+    well.bottom() for well in plate.wells(
+        'E6', 'F6', 'D7', 'E7')]
